2080c08bde00c7174b10cf99ff4517a581b6d8ed
Jiseok Son <jiseokson@gmail.com>
2021-06-08 20:24:03 +0900

    Dragon curve: n세대, p인덱스 문자 탐색하는 함수로 풀이, 미완성

--- 0607-dp-dragon-ans.cpp
+++ 0607-dp-dragon-ans.cpp
@@ -2,17 +2,44 @@
 
 using namespace std;
 
-int c, n, p, l;
+typedef long long ll;
 
-char dragon(string seed, int gen) {
-    //
+void dragon(string seed, int gen, int p) {
+    if (gen == 0) {
+        cout << seed[p - 1];
+        return ;
+    }
+
+    for (char c : seed) {
+        if (c == 'X') {
+            int temp = (3 * ((ll)1 << gen) - 2);
+            if (temp < p) p -= temp;
+            else dragon("X+YF", gen - 1, p);
+        } else if (c == 'Y') {
+            int temp = ((3 * ((ll)1 << gen - 1) - 2));
+            if (temp < p) p -= temp;
+            else dragon("FX-Y", gen - 1, p);
+        } else if (p > 1) --p;
+        else {
+            cout << c;
+            return ;
+        }
+    }
 }
 
 int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int c, n, p, l;
     cin >> c;
 
     while (c--) {
         cin >> n >> p >> l;
+        for (int i = 0; i < l; ++i)
+            dragon("FX", n, p + i);
+        cout << '\n';
     }
 
     return 0;
