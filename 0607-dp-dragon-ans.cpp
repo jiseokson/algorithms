bacd18d44bd64bd42e562caf7ab29f933da78c8b
Jiseok Son <jiseokson@gmail.com>
2021-06-09 20:04:40 +0900

    Dragon curve 풀이
    1. dragon_len(seed, gen): seed를 gen세대 진화시켰을때 얻을수 있는
    문자열의 길이를 반환. 값의 범위를 고려하여 1000000000 기준으로 최소화.
    
    2. dragon(seed, gen, p): seed를 gen세대 진화시켰을때 얻을수 있는
    문자열중 p번째 문자를 출력. k번째 답을 구하는 DP 테크닉을 활용하여 구현.
    
    Question: gen > 0 이고 p == 1 인 경우의 의미?? gen세대 진화한 문자열이
    아니므로 진입하면 안되는 조건 아닌가????

--- 0607-dp-dragon-ans.cpp
+++ 0607-dp-dragon-ans.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <vector>
+
+#define MAX_LEN 1000000000
+#define INIT_SEED 0
+#define EXPANDED_X 1
+#define EXPANDED_Y 2
 
 using namespace std;
 
 typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+vvll dp;
+
+int dragon_len(int seed, int gen) {
+    if (gen == 0) return seed == INIT_SEED ? 2 : 4;
+    ll &ret = dp[seed][gen];
+    if (ret != -1) return ret;
+    if (seed == INIT_SEED) ret = 1 + dragon_len(EXPANDED_X, gen - 1);
+    else ret = dragon_len(EXPANDED_X, gen - 1) + dragon_len(EXPANDED_Y, gen - 1) + 2;
+    return ret = min<ll>(ret, MAX_LEN);
+}
 
 void dragon(string seed, int gen, int p) {
     if (gen == 0) {
@@ -10,16 +29,25 @@ void dragon(string seed, int gen, int p) {
         return ;
     }
 
-    for (char c : seed) {
+    for (char c: seed) {
         if (c == 'X') {
-            int temp = (3 * ((ll)1 << gen) - 2);
-            if (temp < p) p -= temp;
-            else dragon("X+YF", gen - 1, p);
+            int temp = dragon_len(EXPANDED_X, gen - 1);
+            if (temp < p)
+                p -= temp;
+            else {
+                dragon("X+YF", gen - 1, p);
+                return ;
+            }
         } else if (c == 'Y') {
-            int temp = ((3 * ((ll)1 << gen - 1) - 2));
-            if (temp < p) p -= temp;
-            else dragon("FX-Y", gen - 1, p);
-        } else if (p > 1) --p;
+            int temp = dragon_len(EXPANDED_Y, gen - 1);
+            if (temp < p)
+                p -= temp;
+            else {
+                dragon("FX-Y", gen - 1, p);
+                return ;
+            }
+        } else if (p > 1)
+            --p;
         else {
             cout << c;
             return ;
@@ -32,15 +60,19 @@ int main(void) {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int c, n, p, l;
+    int c;
     cin >> c;
 
     while (c--) {
+        int n, p, l;
         cin >> n >> p >> l;
+
+        dp = vvll(3, vll(n + 1, -1));
+
         for (int i = 0; i < l; ++i)
             dragon("FX", n, p + i);
         cout << '\n';
     }
-
+    
     return 0;
 }
