114920d9e481ba89e9803b6ef97f9d06bd5c8f40
Jiseok Son <jiseokson@gmail.com>
2021-06-13 14:09:30 +0900

    WEBBAZIM 풀이 완료!!
    1. 부분 문제 정의:
    dp[less, mod, check] =
      if less: sigma(dp[true, mod - d[j] * dec[j], check + 2^j])
      else:    sigma(dp[ if E[i] == d[j]: false, elif E[i] > d[j]: true, ~, ~])
    
    2. main:ln39:
     -1. j가 0이라면 선택가능
     -2. j가 0초과이지만 이전에 선택된 d와 다르다면 현재 선택가능
     -3. j가 0초과이고 이전에 선택된 d와 현재 선택할 d가 같지만 바로 직전에 이미 선택됐다면 현재 선택가능
         (가격판에 중복되는 숫자가 존재할 수 있어 숫자를 정렬후 순서를 강제하여 선택하도록 하여 경우의 수 중복을 제거)

--- 0613-dp-webbazim-ans.cpp
+++ 0613-dp-webbazim-ans.cpp
@@ -10,29 +10,44 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<vvi> vvvi;
 
-int c, m;
+// var:
+// E: 최초에 입력받은 가격판
+// digit: 가격판에 존재하는 모든 숫자(중복 가능, 정렬)
+// digit_mod = digit % m
+// dec_mod = 10^i % m
+//
+// digit_mod * dec_mod: 10^i 위치의 숫자 선택했을때 mod m
+int c, m, n;
 string e;
-
-vi E, Emod, Dmod;
+vi E, digit, digit_mod, dec_mod;
 vvvi dp;
 
-int solve(int i, int mod, bool less, int check) {
-    if (i == E.size())
-        return mod % m == 0? 1: 0;
+// i: 현재 선택할 인덱스
+// less: 이전까지 선택된 숫자가 최초의 E보다 작은지 여부
+// mod: 이후 선택에서의 mod m
+// check: 이전까지 선택된 숫자의 집합
+int solve(int i, bool less, int mod, int check) {
+    if (i == n)
+        return less && mod % m == 0? 1: 0;
 
-    int &ret = dp[mod][less][check];
+    int &ret = dp[less][mod][check];
     if (ret != -1) return ret;
 
     ret = 0;
-    for (int j = 0; j < E.size(); ++j) {
-        if (less) {
-            if (check && (1 << j)) continue;
-            ret = (ret + solve(i + 1, (m + mod - (Emod[j] * Dmod[i]) % m) % m, less, check + (1 << j))) % MOD;
-        } else {
-            if ()
-            //
+    for (int j = 0; j < n; ++j) {
+        if (check & (1 << j)) continue;
+        if (j > 0 && digit[j - 1] == digit[j] && (~check & (1 << (j - 1)))) continue;
+        int next_mod = (m + mod - (digit_mod[j] * dec_mod[i]) % m) % m;
+        if (less)
+            ret = (ret + solve(i + 1, true, next_mod, check + (1 << j))) % MOD;
+        else {
+            if (E[i] == digit[j])
+                ret = (ret + solve(i + 1, false, next_mod, check + (1 << j))) % MOD;
+            else if (E[i] > digit[j])
+                ret = (ret + solve(i + 1, true, next_mod, check + (1 << j))) % MOD;
         }
     }
+    return ret;
 }
 
 int main(void) {
@@ -44,21 +59,25 @@ int main(void) {
 
     while (c--) {
         cin >> e >> m;
+        n = e.length();
 
-        E = vi(), Emod = vi();
+        E = vi();
         for (char d: e)
             E.push_back(d - '0');
-        sort(E.begin(), E.end());
-        for (int d: E)
-            Emod.push_back(d % m);
 
-        Dmod = vi(E.size(), 1);
+        digit = vi(E.begin(), E.end());
+        sort(digit.begin(), digit.end());
+
+        digit_mod = vi(digit.begin(), digit.end());
+        for (int &dm: digit_mod) dm = dm % m;
+
+        dec_mod = vi(n, 1);
         int step = 10 % m;
-        for (auto i = Dmod.rbegin() + 1; i != Dmod.rend(); ++i)
+        for (auto i = dec_mod.rbegin() + 1; i != dec_mod.rend(); ++i)
             *i = *(i - 1) * step % m;
 
         dp = vvvi(2, vvi(m, vi(1 << E.size(), -1)));
-        cout << solve(0, 0, false, 0); << '\n';
+        cout << solve(0, false, 0, 0) << '\n';
     }
 
     return 0;
