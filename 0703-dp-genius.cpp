0a3746842e20d9e3081a28491dbc25fd23e3bade
Jiseok Son <jiseokson@gmail.com>
2021-07-09 20:30:04 +0900

    Genius 풀이!! Dynamic Programming 끝!
    1. 부분문제 정의:
    dp[t, now]: t시간에 now가 재생될 확률
    dp[t, now] = sigma(dp[t - L[prev], now) * T[prev, now]
    시간복잡도: O(kn^2)
    
    k <= 1,000,000에서는 너무 큰 복잡도!! >> 따라서 최적화필요..
    
    2. 부분문제의 특징: 한 부분문제의 정답이 다른 부분문제의 선형결합으로
    표현됨. 이런 특징을 활용해 행렬 형태로 부분문제 여럿을 한번에 표현, 곱을
    통해 더 빠른 속도로 계산하자.
    
    3. 행렬표현:
     1). 열벡터 [dp[t, 0] ~ dp[t, n - 1], dp[t - 1, 0] ~ dp[t - 1, n - 1]
    ~ dp[t - 3, 0] ~ dp[t - 3, n - 1]]
     2). W^k * m0에서 dp[~, q]에 해당하는 값들을 누산하여 출력

--- 0703-dp-genius.cpp
+++ 0703-dp-genius.cpp
@@ -2,27 +2,50 @@
 #include <vector>
 using namespace std;
 
-typedef vector<int> vi;
 typedef vector<double> vd;
 typedef vector<vd> vvd;
 
-int c, n, k, m;
-int target;
-vi l;
-vvd T, dp;
+typedef vector<int> vi;
 
-double solve(int t, int prev) {
-    if (prev == target && t - l[prev] <= k && k < t) return 1;
-    if (k < t) return 0;
-    double &res = dp[t][prev];
-    if (res != -1.0) return res;
+class MatrixRow {
+public:
+    vd row;
+    const int col;
+    MatrixRow(int col): col(col), row(col, 0.0) {};
+    double& operator[](int i) { return row[i]; }
+};
 
-    res = 0.0;
-    // for (int next = 0; next < n; ++n)
-    for (int next = 0; next < n; ++next)
-        res += T[prev][next] * solve(t + l[next], next);
-    return res;
-}
+class Matrix {
+    public:
+    vector<MatrixRow> mat;
+    const int row, col;
+
+    Matrix(int row, int col): row(row), col(col), mat(row, MatrixRow(col)) {}
+
+    MatrixRow& operator[](int i) { return mat[i]; }
+
+    Matrix operator*(Matrix& rhs) {
+        Matrix res(row, rhs.col);
+        for (int i = 0; i < row; ++i)
+            for (int j = 0; j < rhs.col; ++j)
+                for (int k = 0; k < col; ++k)
+                    res[i][j] += (*this)[i][k] * rhs[k][j];
+        return res;
+    }
+
+    Matrix pow(int n) {
+        if (row != col) return Matrix(1, 1);
+
+        Matrix res(row, row);
+        if (n == 0) {
+            for (int i = 0; i < row; ++i) res[i][i] = 1.0;
+            return res;
+        }
+
+        Matrix temp = pow(n / 2);
+        return n % 2 == 0? temp * temp: temp * temp * (*this);
+    }
+};
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -32,20 +55,36 @@ int main(void) {
     cout << fixed;
     cout.precision(8);
 
+    int c;
     cin >> c;
 
     while (c--) {
+        int n, k, m;
         cin >> n >> k >> m;
-        l = vi(n);
-        T = vvd(n, vd(n));
-        for (int i = 0; i < n; ++i) cin >> l[i];
+        vi L(n);
+        Matrix T(n, n);
+
+        for (int i = 0; i < n; ++i) cin >> L[i];
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; ++j)
                 cin >> T[i][j];
+
+        Matrix W(4 * n, 4 * n);
+        Matrix m0(4 * n, 1);
+        m0[0][0] = 1.0;
+        for (int r = 0; r < 3 * n; ++r) W[n + r][r] = 1.0;
+        for (int prev = 0; prev < n; ++prev)
+            for (int next = 0; next < n; ++next)
+                W[next][(L[prev] - 1) * n + prev] = T[prev][next];
+        Matrix ans = W.pow(k) * m0;
+
         for (int i = 0; i < m; ++i) {
-            cin >> target;
-            dp = vvd(1000000, vd(n, -1.0));
-            cout << solve(k - l[0], 0) << ' ';
+            int q;
+            cin >> q;
+            double res = 0.0;
+            for (int j = 0; j < L[q]; ++j)
+                res += ans[n * j + q][0];
+            cout << res << ' ';
         }
         cout << '\n';
     }
