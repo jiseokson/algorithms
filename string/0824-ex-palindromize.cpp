d3c5ee1f1ec3e801259cf17a5bd3aa7d004fb6a1
Jiseok Son <jiseokson@gmail.com>
2021-08-24 19:14:58 +0900

    Palindromize 풀이

--- string/0824-ex-palindromize.cpp
+++ string/0824-ex-palindromize.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 #include <vector>
+#include <string>
 
 using namespace std;
 using vi = vector<int>;
 
-int getFirstMath(const string s, const string r) {
-    //
+vi getPi(const string &p) {
+    vi pi = vi(p.length(), 0);
+    int j = 0;
+    for (int i = 1; i < p.length(); ++i) {
+        while (j > 0 && p[i] != p[j])
+            j = pi[j - 1];
+        if (p[i] == p[j])
+            pi[i] = ++j;
+    }
+    return pi;
+}
+
+int getMaxOverlap(const string &str, const string &rev) {
+    vi pi = getPi(rev);
+    int j = 0;
+    for (int i = 0; i < str.length(); ++i) {
+        while (j > 0 && str[i] != rev[j])
+            j = pi[j - 1];
+        if (str[i] == rev[j]) {
+            ++j;
+            if (j == rev.length())
+                return rev.length();
+        }
+    }
+    return j;
 }
 
 int main(void) {
     string str;
     cin >> str;
-    string reversed = str;
-    reverse(reversed.begin(), reversed.end());
-    int first = getFirstMatch(str, reversed);
-    cout << first << endl;
-    cout  << string(str.begin(), str.begin() + first) << reversed << endl;
+    string rev = string(str.rbegin(), str.rend());
+    int overlap = getMaxOverlap(str, rev);
+    cout << string(str.begin(), str.end() - overlap) << rev << endl;
     return 0;
 }
