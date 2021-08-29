9af5b0dd06d020e3cb0a859e50fd013fdcfc1f30
Jiseok Son <jiseokson@gmail.com>
2021-08-29 19:47:59 +0900

    Fortress: getTree잘못된 구현

--- tree/0829-fortress-2.cpp
+++ tree/0829-fortress-2.cpp
@@ -15,24 +15,26 @@ inline int square(int a) {
 }
 
 inline bool enclose(Node *a, Node *b) {
-    return square(a->x - b->x) + square(a->y - b->y) < square(a->r - b->r);
+    return a->r >= b->r && square(a->x - b->x) + square(a->y - b->y) <= square(a->r - b->r);
 }
 
 Node *getTree(int n) {
     Node *root = new Node();
     cin >> root->x >> root->y >> root->r;
     vector<Node*> nodes(1, root);
-    for (int i = 1; i < n; ++i) {
-        Node *newNode = new Node();
-        cin >> newNode->x >> newNode->y >> newNode->r;
-        Node *parent = root;
-        for (Node *parentCand: nodes)
-            if (enclose(parent, parentCand) && enclose(parentCand, newNode))
-                parent = parentCand;
-        parent->child.push_back(newNode);
-        nodes.push_back(newNode);
-    }
-    return root;
+    
+
+    // for (int i = 1; i < n; ++i) {
+    //     Node *newNode = new Node();
+    //     cin >> newNode->x >> newNode->y >> newNode->r;
+    //     Node *parent = root;
+    //     for (Node *parentCand: nodes)
+    //         if (enclose(parent, parentCand) && enclose(parentCand, newNode))
+    //             parent = parentCand;
+    //     parent->child.push_back(newNode);
+    //     nodes.push_back(newNode);
+    // }
+    // return root;
 }
 
 void deleteTree(Node *tree) {
@@ -42,7 +44,6 @@ void deleteTree(Node *tree) {
 }
 
 int depth(Node *node) {
-    if (node == nullptr) return 0;
     int d = 0;
     for (Node *c: node->child) d = max(d, depth(c) + 1);
     return d;
@@ -56,10 +57,24 @@ int maxLen(Node *root) {
     return *depths.rbegin();
 }
 
+void print(Node *tree, int d = 0) {
+    for (int i = 0; i < d; ++i) cout << ' ' ;
+    cout << tree->x << ' ' << tree->y << ' ' << tree->r << '\n';
+    for (Node *c: tree->child) print(c, d + 1);
+}
+
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    // Node *n1 = new Node();
+    // Node *n2 = new Node();
+    // n1->x = 5, n1->y=5, n1->r = 5;
+    // n2->x = 5, n2->y=5, n2->r = 5;
+    // cout << enclose(n1, n2) << '\n';
+    // cout << enclose(n2, n1) << '\n';
+    // return 0;
 
     int tc;
     cin >> tc;
@@ -68,6 +83,7 @@ int main(void) {
         int n;
         cin >> n;
         Node *root = getTree(n);
+        print(root);
         cout << maxLen(root) << '\n';
         deleteTree(root);
     }
