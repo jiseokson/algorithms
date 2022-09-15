e50b5a0846f35652aa1a29dddc1f13a1ef574b29
Jiseok Son <jiseokson@gmail.com>
2022-09-15 21:42:18 +0900

    Fanmeeting 풀이
    실패원인:
    1) 입력 중 하나를 뒤집어야함.
    2) 곱셈에서 자리올림을 구현함. 올바른 구현을 위해선 불필요함.
    3) karatsuba 알고리즘의 기저사례인 입력의 크기가 충분히 작은 경우의
       기준을 너무 작게 설정함.

--- codes/ch07/0915-03-fanmeeting.cpp
+++ codes/ch07/0915-03-fanmeeting.cpp
@@ -6,42 +6,17 @@ using namespace std;
 
 using vi = vector<int>;
 
-vi to_vi(const string& str) {
-    vi a;
-    for (int i = 0; i < str.length(); ++i)
-        a.push_back(str[i] == 'F'? 0: 1);
-    return a;
-}
-
-void normalize(vi& num) {
-    num.push_back(0);
-    for (int i = 0; i + 1 < num.size(); ++i) {
-        if (num[i] < 0) {
-            int borrow = (abs(num[i]) + 9) / 10;
-            num[i + 1] -= borrow;
-            num[i] += borrow * 10;
-        }
-        else {
-            num[i + 1] += num[i] / 10;
-            num[i] %= 10;
-        }
-    }
-    while (num.size() > 1 && num.back() == 0) num.pop_back();
-}
-
 void add_to(vi& a, const vi& b, int k) {
     // a += b * 10^k
     a.resize(max(a.size(), b.size() + k) + 1, 0);
     for (int i = 0; i < b.size(); ++i)
         a[i + k] += b[i];
-    normalize(a);   
 }
 
 void sub_from(vi& a, const vi& b) {
     // a >= b 가정, a -= b
     for (int i = 0;  i < b.size(); ++i)
         a[i] -= b[i];
-    normalize(a);
 }
 
 vi multiply(const vi& a, const vi& b) {
@@ -49,7 +24,6 @@ vi multiply(const vi& a, const vi& b) {
     for (int i = 0; i < a.size(); ++i)
         for (int j = 0; j < b.size(); ++j)
             product[i + j] += a[i] * b[j];
-    normalize(product);
     return product;
 }
 
@@ -57,7 +31,7 @@ vi karatsuba(const vi& a, const vi& b) {
     int an = a.size(), bn = b.size();
     if (an < bn) return karatsuba(b, a);
     if (an == 0 || bn == 0) return vi(1, 0);
-    if (an <= 20) return multiply(a, b);
+    if (an <= 50) return multiply(a, b);
 
     int half = an / 2;
     vi a0 = vi(a.begin(), a.begin() + half);
@@ -99,10 +73,14 @@ int main(void) {
     while (c--) {
         string members, fans;
         cin >> members >> fans;
-        reverse(members.begin(), members.end());
-        vi result = karatsuba(to_vi(members), to_vi(fans));
+
+        vi m(members.length()), f(fans.length());
+        for (int i = 0; i < m.size(); ++i) m[m.size() - i - 1] = (members[i] == 'M');
+        for (int i = 0; i < f.size(); ++i) f[i] = (fans[i] == 'M');
+
+        vi result = karatsuba(m, f);
         result.resize(members.length() + fans.length() - 1, 0);
-        cout << count_zero(result, members.length() - 1, result.size() - members.length()) << '\n';
+        cout << count_zero(result, members.length() - 1, fans.length() - 1) << '\n';
     }
 
     return 0;
