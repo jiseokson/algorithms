5901da478bec066ac513847ceccfaf269cf89fe4
Jiseok Son <jiseokson@gmail.com>
2022-09-17 21:17:40 +0900

    wildcard풀이 + dp 예제

--- codes/ch08/0915-01-wildcard.cpp
+++ codes/ch08/0915-01-wildcard.cpp
@@ -12,22 +12,34 @@ int c, n;
 string wildcard, filename;
 vvi cache;
 
-int match(int i, int j) {
-    if (i == wildcard.length() && j == filename.length())
-        return 1;
+// int match(int i, int j) {
+//     if (i == wildcard.length() && j == filename.length())
+//         return 1;
+//     if (i == wildcard.length() || j == filename.length())
+//         return 0;
 
+
+//     int& ret = cache[i][j];
+//     if (ret != -1) return ret;
+
+//     if (wildcard[i] == '?')
+//         return ret = match(i + 1, j + 1);
+//     if (wildcard[i] != '*')
+//         return ret = (wildcard[i] == filename[j]? match(i + 1, j + 1): 0);
+//     return ret = match(i, j + 1) || match(i + 1, j);
+// }
+
+int match(int i, int j) {
     int& ret = cache[i][j];
     if (ret != -1) return ret;
 
-    if (wildcard[i] == '?')
+    if (i == wildcard.length()) return ret = (j == filename.length());
+    if (i < wildcard.length() && j < filename.length() && (wildcard[i] == '?' || wildcard[i] == filename[j]))
         return ret = match(i + 1, j + 1);
-    if (wildcard[i] != '*')
-        return ret = (wildcard[i] == filename[j]? match(i + 1, j + 1): 0);
-    for (; i < wildcard.length() && wildcard[i] == '*'; ++i);
-    for (; j <= filename.length(); ++j)
-        if (match(i, j)) return ret = 1;
+    if (wildcard[i] == '*') return ret = match(i + 1, j) || (j < filename.length() && match(i, j + 1));
     return ret = 0;
 }
+
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -40,7 +52,8 @@ int main(void) {
         vs matches;
         for (int i = 0; i < n; ++i) {
             cin >> filename;
-            cache = vvi(wildcard.length() + 1, vi(filename.length() + 1, -1));
+            // cache = vvi(wildcard.length() + 1, vi(filename.length() + 1, -1));
+            cache = vvi(250, vi(250, -1));
             if (match(0, 0)) matches.push_back(filename);
         }
         sort(matches.begin(), matches.end());
