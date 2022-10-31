83b3b3f9b3e446743a566da2810f2bc433a89ef1
Jiseok Son <jiseokson@gmail.com>
2022-10-31 15:11:25 +0900

    packing 풀이:
    음의 무한 neg_inf 도입하여 부분문제 구현 변경
    reconstruct에서 choice배열 이용하지 않고 부분문제 답을 이용해 각
    단계에서의 선택 추적하도록 변경(각 단계에서 가능한 선택의 경우의 수가 2가지
    뿐이므로)

--- codes/ch09/1031-01-packing.cpp
+++ codes/ch09/1031-01-packing.cpp
@@ -7,34 +7,31 @@ using vi = vector<int>;
 using vvi = vector<vi>;
 using vs = vector<string>;
 
+const int neg_inf = -987654321;
+
 int N, W;
 vs stuff;
 vi w, p;
-vvi cache, choice;
+vvi cache;
 
 int pack(int i, int v)
 {
+    if (v < 0) return neg_inf;
     if (i == N) return 0;
     int &ret = cache[i][v];
     if (ret != -1) return ret;
-    ret = pack(i + 1, v);
-    choice[i][v] = v;
-    if (v - w[i] >= 0) {
-        int temp = p[i] + pack(i + 1, v - w[i]);
-        if (ret < temp) {
-            ret = temp;
-            choice[i][v] = v - w[i];
-        }
-    }
-    return ret;
+    return ret = max(pack(i + 1, v), p[i] + pack(i + 1, v - w[i]));
 }
 
 void reconstruct(vs &packs, int i, int v)
 {
     if (i == N) return;
-    if (choice[i][v] != v)
+    if (pack(i, v) == pack(i + 1, v)) {
+        reconstruct(packs, i + 1, v);
+    } else {
         packs.push_back(stuff[i]);
-    reconstruct(packs, i + 1, choice[i][v]);
+        reconstruct(packs, i + 1, v - w[i]);
+    }
 }
 
 int main(void)
@@ -52,15 +49,14 @@ int main(void)
         w = vi(N);
         p = vi(N);
         cache = vvi(N + 1, vi(W + 1, -1));
-        choice = vvi(N + 1, vi(W + 1, -1));
         for (int i = 0; i < N; ++i)
             cin >> stuff[i] >> w[i] >> p[i];
         vs packs;
-        int max_p = pack(0, W);
         reconstruct(packs, 0, W);
-        cout << max_p << " " << packs.size() << "\n";
+        cout << pack(0, W) << " " << packs.size() << "\n";
         for (int i = 0; i < packs.size(); ++i)
             cout << packs[i] << "\n";
     }
+
     return 0;
 }
